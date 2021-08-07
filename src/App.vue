b720cbc490a38eabc0d7579730c156234b381976
ohyama4z <45777685+ohyama4z@users.noreply.github.com>
2021-08-07 18:58:45 +0900

    eslint

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+<Todo />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import Todo from './components/Todo.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    Todo
   }
 })
 </script>
